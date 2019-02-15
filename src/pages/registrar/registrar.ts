1dd22ea82f3302bcb3040a000a307d49489cd745
kevinbonillas <kevinbonillas@users.noreply.github.com>
2019-02-14 22:07:23 -0400

    validacion registro

--- src/pages/registrar/registrar.ts
+++ src/pages/registrar/registrar.ts
@@ -6,6 +6,7 @@ import { firebaseService } from '../../services/firebase.service';
 import { LoginPage } from '../login/login';
 import { AlertController } from 'ionic-angular';
 
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'; // Para la validacion del formulario
 /**
  * Generated class for the RegistrarPage page.
  *
@@ -24,24 +25,56 @@ import { mysqlService } from '../../services/mysql.service';
 export class RegistrarPage {
 
 
-
   dataMYSQL={
     carnet:0,
     nombre:'',
     apellido:'',
     pass1:'',
-    fecha_nac:this.dameFecha(),
-    tipo:'C',
+    fecha_nac:'',
+    tipo:'',
     telf:0
   };
 
+  submitAttempt: boolean = false;
+  myForm: FormGroup;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public servicio:firebaseService
   ,public alerta:AlertController,private afAauth:AngularFireAuth,private platform:Platform,
-    public mysql:mysqlService) {
-    this.platform.registerBackButtonAction(() => {
-      console.log('');
-    },10000);
+    public mysql:mysqlService, public formBuilder: FormBuilder) {
+
+      this.platform.registerBackButtonAction(() => {
+        console.log('');
+      },10000);
+    
+      this.myForm = this.formBuilder.group({
+        nombre: ['', Validators.compose([Validators.maxLength(20),Validators.required])],
+        apellido: ['', Validators.compose([Validators.maxLength(25),Validators.required])],
+        carnet: ['', Validators.compose([Validators.maxLength(10), Validators.required])],
+        fecha_nac: ['', Validators.compose([ Validators.required])],
+        //p_email: ['', Validators.compose([Validators.required])],
+        //password_ctrl: this.formBuilder.group({
+        pass1: ['', [Validators.pattern(/^[a-z0-9_-]{6,18}$/)]],
+        tipo:'',
+        confirm_password: ['', [Validators.pattern(/^[a-z0-9_-]{6,18}$/)]],
+        telefono: ['', Validators.compose([Validators.maxLength(8), Validators.required])]
+        //}, this.matchPassword)
+      }, {'validator': this.matchingPasswords('pass1', 'confirm_password')});
+
+  }
+
+  
+  matchingPasswords(passwordKey: string, confirmPasswordKey: string) {
+    // TODO maybe use this https://github.com/yuyang041060120/ng2-validation#notequalto-1
+    return (group: FormGroup): {[key: string]: any} => {
+      let password = group.controls[passwordKey];
+      let confirm_password = group.controls[confirmPasswordKey];
+
+      if (password.value !== confirm_password.value) {
+        return {
+          mismatchedPasswords: true
+        };
+      }
+    }
   }
 
   /*ionViewDidLoad(){
@@ -49,12 +82,11 @@ export class RegistrarPage {
   }*/
 
 
-  registrar(Usuario)
+  registrar()
   {
     let info={};
-    console.log(Usuario);
-
-    this.mysql.AgregarUsuario(Usuario).subscribe(
+    
+    this.mysql.AgregarUsuario(this.myForm.value).subscribe(
       data => {
         console.log('data', data);
         info= Object.assign(data);
